59a4d726fdbf0219174f840c056af605b397c3bd
Chr1sChen <Chr1sChen@users.noreply.github.com>
2018-11-17 17:17:02 +0000

    Changes
    Changes in css file, attractions and requesting page, and python programme

--- holiday rental.py
+++ holiday rental.py
@@ -1,12 +1,47 @@
+import csv
 from flask import Flask, render_template
+from flask import request
 app = Flask(__name__)
+def readFile(aFile):
+    with open(aFile, 'r') as inFile:
+         reader = csv.reader(inFile) 
+         skillList = [row for row in reader]
+    return skillList
+def writeFile(aList,aFile):
+    with open(aFile, 'w', newline='') as outFile: 
+        write=csv.writer(outFile)
+        write.writerows(aList)
+    return
 @app.route('/')
-	def home():
-	return render_template('home.html')
-	
-	
-	
-	
-	
+def home(): 
+    return render_template('home.html')
+
+
+@app.route('/skills', methods = ['GET'])
+def skills():
+    skillFile='static\\skills.csv'
+    skillList= readFile(skillFile)
+    return render_template('skills.html', skillList=skillList)
+
+@app.route('/addSkill',methods= ['POST'])
+def addSkill():
+    skillName= request.form[('skillName')]
+    rating= request.form[('rating')]
+    newSkill=[skillName,rating]
+    skillFile='static\\skills.csv'
+    skillList= readFile(skillFile)
+    skillList.append(newSkill)
+    
+    writeFile(skillList,skillFile)
+    
+    return render_template('skills.html', skillList=skillList)
+    
+    
+    
+
+    
+
+
+    
 if __name__ == '__main__':
- app.run(debug = True)
+    app.run(debug = True)
