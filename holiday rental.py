e17f32c23b7771eab3ef4e922442b2e2f3e498d6
Chr1sChen <Chr1sChen@users.noreply.github.com>
2018-11-17 17:26:43 +0000

    add csv file
    add csv file and pasted the right py file

--- holiday rental.py
+++ holiday rental.py
@@ -1,47 +1,36 @@
-import csv
 from flask import Flask, render_template
-from flask import request
 app = Flask(__name__)
-def readFile(aFile):
-    with open(aFile, 'r') as inFile:
-         reader = csv.reader(inFile) 
-         skillList = [row for row in reader]
-    return skillList
+@app.route('/')
+	def home():
+	return render_template('home.html')
+
+#Set file reading and writing function	
 def writeFile(aList,aFile):
     with open(aFile, 'w', newline='') as outFile: 
         write=csv.writer(outFile)
         write.writerows(aList)
-    return
-@app.route('/')
-def home(): 
-    return render_template('home.html')
-
-
-@app.route('/skills', methods = ['GET'])
-def skills():
-    skillFile='static\\skills.csv'
-    skillList= readFile(skillFile)
-    return render_template('skills.html', skillList=skillList)
-
-@app.route('/addSkill',methods= ['POST'])
-def addSkill():
-    skillName= request.form[('skillName')]
-    rating= request.form[('rating')]
-    newSkill=[skillName,rating]
-    skillFile='static\\skills.csv'
-    skillList= readFile(skillFile)
-    skillList.append(newSkill)
-    
-    writeFile(skillList,skillFile)
-    
-    return render_template('skills.html', skillList=skillList)
-    
-    
-    
-
-    
-
-
-    
+	return
+def readFile(aFile):
+    with open(aFile, 'r') as inFile:
+         reader = csv.reader(inFile) 
+         detailList = [row for row in reader]
+    return detailList
+	
+@app.route('/addDetails')
+		def addDetails:
+			checkIn=requet.form[('checkIn')]
+			checkOut=requet.form[('checkOut')]
+			title=requet.form[('title')]
+			firstName=requet.form[('firstName')]
+			adultsNumbers=requet.form[('adultsNumbers')]
+			childrenNumbers=requet.form[('childrenNumbers')]
+			email=requet.form[('email')]
+			newDetail=[checkIn,checkOut,title,firstName,adultsNumbers,childrenNumbers,email]	
+			fileName='static\\requestDetail.csv'
+			detailList=readFile(fileName)
+			detailList.append(newDetail)
+			
+			writeFile(detailList,fileName)
+			return render_template('requesting.html',detailList =detailList)
 if __name__ == '__main__':
-    app.run(debug = True)
+ app.run(debug = True)
