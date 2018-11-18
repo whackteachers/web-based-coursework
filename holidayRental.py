6a0c63fb6b6fca5198b005e6131d5e1606007d7e
Chr1sChen <Chr1sChen@users.noreply.github.com>
2018-11-18 02:08:43 +0000

    change

---
+++ holidayRental.py
@@ -0,0 +1,44 @@
+import csv
+from flask import Flask, render_template
+from flask import request
+app = Flask(__name__)
+@app.route('/')
+def home(): 
+    return render_template('home.html')
+
+#Set file reading and writing function	
+
+def readFile(aFile):
+    with open(aFile, 'r') as inFile:
+         reader = csv.reader(inFile) 
+         detailList = [row for row in reader]
+    return detailList
+	
+	
+def writeFile(aList,aFile):
+     with open(aFile, 'w', newline='') as outFile: 
+        write=csv.writer(outFile)
+        write.writerows(aList)
+     return	
+	 
+	 
+@app.route('/addDetails')
+def addDetails():
+	checkIn=request.form[('checkIn')]
+	checkOut=request.form[('checkOut')]
+	title=request.form[('title')]
+	firstName=request.form[('firstName')]
+	adultsNumbers=request.form[('adultsNumbers')]
+	childrenNumbers=request.form[('childrenNumbers')]
+	email=request.form[('email')]
+	newDetail=[checkIn,checkOut,title,firstName,adultsNumbers,childrenNumbers,email]	
+	fileName='static\\skills.csv'
+	detailList=readFile(fileName)
+	detailList.append(newDetail)
+			
+	writeFile(detailList,fileName)
+	return render_template('requesting.html',detailList =detailList)
+	
+	
+if __name__ == '__main__':
+ app.run(debug = True)
