cbbf82a338f75c554c97ec7a9604dd2e1938b264
whackteachers <33785383+whackteachers@users.noreply.github.com>
2018-11-23 00:49:42 +0000

    organised facilities rooms part
    gallery still broken

--- static/gallery.js
+++ static/gallery.js
@@ -13,7 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("photo");
+  var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
