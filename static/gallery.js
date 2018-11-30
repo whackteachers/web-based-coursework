2cf9002238b87312927cd713868f71ff9b2e6804
whackteachers <33785383+whackteachers@users.noreply.github.com>
2018-11-30 00:02:10 +0000

    further edit on homepage top

--- static/gallery.js
+++ static/gallery.js
@@ -15,16 +15,19 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+  //slideIndex = (n % slides.length) + 1;
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    //if(i== slideIndex){
+    //  slides[slideIndex-1].style.display = "block";
+    //}else{
+      slides[i].style.display = "none";
+    //}
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
