06cbbd732cdafb93b37c3987929ea1a9f62af31d
Chr1sChen <Chr1sChen@users.noreply.github.com>
2018-12-04 10:36:17 +0000

    fix the second datepicker

--- static/datepicker.js
+++ static/datepicker.js
@@ -1,19 +1,17 @@
-$( function() {
-	var ukFormat = "dd/mm/yy",
+  $( function() {
+    var dateFormat = "dd/mm/yy",
 	tet = ["18/12/2018","19/12/2018"]
 	
 	//choosing the check in date
       from = $( "#from" )
-		.datepicker(
-			{
-			defaultDate: "+1w",
-			changeMonth: true,
-			numberOfMonths: 1,
-			minDate: 0,
-			dateFormat: ukFormat,
-			beforeShowDay: function(date){
+        .datepicker({
+		  minDate: 0,
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 1,
+		  beforeShowDay: function(date){
 				for (var i = 0; i < tet.length; i++){
-					var s = jQuery.datepicker.formatDate(ukFormat, date);
+					var s = jQuery.datepicker.formatDate(dateFormat, date);
 					var booked = tet.indexOf(s) == -1 ;
 					if(!booked){
 						return [false , "reserved","booking"]
@@ -23,30 +21,34 @@ $( function() {
 					
 					}
 				}
-			}
-		)
-        .on( "change", 
-			function() {
-				to.datepicker( "option", "minDate", getDate( this ) );
-			}
-		),
-	  //choosing the check in date
-      to = $( "#to" )
-	  .datepicker(
-		{
-			defaultDate: "+1w",
-			changeMonth: true,
-			numberOfMonths: 1,
-			dateFormat: ukFormat,
-		}
-	  )
-      .on( "change", function() 
-		{
-			from.datepicker( "option", "maxDate", getDate( this ) );
-		}
-	  );
+        })
+        .on( "change", function() {
+          to.datepicker( "option", "minDate", getDate( this ) );
+        }),
+      to = $( "#to" ).datepicker({
+		minDate: '1',
+		firstDay: '1',
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 1,
+		beforeShowDay: function(date){
+				for (var i = 0; i < tet.length; i++){
+					var s = jQuery.datepicker.formatDate(dateFormat, date);
+					var booked = tet.indexOf(s) == -1 ;
+					if(!booked){
+						return [false , "reserved","booking"]
+					}else{
+						return [true , '']
+					}
+					
+					}
+				}
+      })
+      .on( "change", function() {
+        from.datepicker( "option", "maxDate", getDate( this ) );
+      });
  
-      function getDate( element ) {
+    function getDate( element ) {
       var date;
       try {
         date = $.datepicker.parseDate( dateFormat, element.value );
@@ -55,6 +57,5 @@ $( function() {
       }
  
       return date;
-	  }
-	} 
-);
+    }
+  } );
